a2451c18e445d51cafafac372f5042208e04deb3
Haykanush Grigoryan <69928077+HaykanushGrigoryan@users.noreply.github.com>
2025-06-13 00:54:04 +0400

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,10 @@
+
+print("It's my first program", end = '!\n' ) 
+print("Haykanush Grigoryan" '\n')
+print("I have a degree in economics, and I am now interested in the field of quality assurance" ) #additional line , in Terminal the summ i see after empty line
+number1 = 2
+number2 = 3
+sum = number1+number2
+print(sum)
+
+
